7a76f16365748a84d655c59417d71b7aec105e7c
Snail63 <149165570+Snail63@users.noreply.github.com>
2023-10-27 20:10:47 +0800

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Snail Tree",
+	id: "SNAILISREAL",
+	author: "Snail63",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
