3a85493bada4cc1b506b1668608449fbe4cceb04
Snail63 <149165570+Snail63@users.noreply.github.com>
2023-10-27 20:49:59 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -25,10 +25,7 @@ addLayer("p", {
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
-    ,         11: {     
-    title: "Double Points",
-    description: "Double your point gain.",
-    cost: new Decimal(1),
+    ,         11: {
 
     },
 })
